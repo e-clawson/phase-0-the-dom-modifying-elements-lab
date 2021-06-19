5925789a0165de82dedc9a303bc89f4b04e65fc8
Elizabeth Clawson <emclawson1@gmail.com>
2021-06-19 16:12:38 -0500

    Cpmleted Assign

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector('main').remove(); 
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+document.querySelector('h#victory');
+newHeader.innerHTML = "NAME is the champion";
+document.body.appendChild(newHeader);
